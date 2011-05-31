fa97481137f154ac27d8681360c43851d1462b7f
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-05-31 12:26:03 +0200

    Add Option usage

--- src/main/java/com/sample/portlet/fwk/Model.java
+++ src/main/java/com/sample/portlet/fwk/Model.java
@@ -1,7 +1,12 @@
 package com.sample.portlet.fwk;
 
+import com.sample.portlet.fwk.F.Maybe;
 import java.util.HashMap;
 
 public class Model extends HashMap<String, Object> {
+
+    public Maybe<Object> forKey(String key) {
+        return new Maybe<Object>(super.get(key));
+    }
 
 }
